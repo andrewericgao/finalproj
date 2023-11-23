6f59175c20af89f3d53c214d941d5578d6d3e276
Andrewegao <andrewgao99@gmail.com>
2023-11-22 21:42:47 -0600

    fixed local

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TextInputForm from './components/TextInputForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Text Sentiment Analysis Dashboard</h1>
+      <TextInputForm />
+      {/* You can add other components here as needed */}
+    </div>
+  );
+}
+
+export default App;
